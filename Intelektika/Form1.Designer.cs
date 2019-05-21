75ab3203aabbc19d6eea7a52191a38eefc7a7403
MissedSte4k <36638029+MissedSte4k@users.noreply.github.com>
2019-05-21 21:31:37 +0300

    added dimensions

--- Intelektika/Form1.Designer.cs
+++ Intelektika/Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace Intelektika
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -42,21 +43,32 @@ namespace Intelektika
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(120, 238);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(499, 142);
+            this.textBox1.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(736, 420);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Intelektika/Form1.cs
+++ Intelektika/Form1.cs
@@ -18,15 +18,24 @@ namespace Intelektika
     {
         public string file = @"..\Debug\Data\games.csv";
 
-        public const int MaxTime = 16;
-        public const int LearnData = 1000;
-        List<List<int>> fullList = new List<List<int>>();
+        public const int MaxTime = 4;
+        public const int LearnData = 40000;
+        List<List<int>> CheckList = new List<List<int>>();
         List<Dictionary<int, Dictionary<int, Instant>>> visas = new List<Dictionary<int, Dictionary<int, Instant>>>();
         public const int MaxNumberOfNormalizedData = 57;
+        public const int MaxNumberOfData = 24;
         public int WinCount = 0;
         public int DefeatCount = 0;
         public double pirmas = 0.5;
-
+        public double slenkstis = 0.5;
+        public int vertinamasTop = 10;
+        public int[] champions = new int[]{11,14,17,20,23,36,39,42,45,48};
+        public int[] bans = new int[] { 31,32,33,34,35,56,57,58,59,60};
+        public int[] summonerSpell = new int[] { 12,13,15,16,18,19,21,22,24,25,37,38,40,4,43,44,46,47,49,50};
+        public int champSpot = 21;
+        public int banSpot = 22;
+        public int summSpot = 23;
+        public int sumazinta = 0;
 
         public Form1()
         {
@@ -42,11 +51,11 @@ namespace Intelektika
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Thanks!");
             var Data = ReadLinesAsString();
-            
-            for (int o = 1; o <= 10; o++)
-            {
+
+            /*for (int o = 1; o <= 10; o++)
+            {*/
+            int o = 1;
                 WinCount = 0;
                 DefeatCount = 0;
                 visas = new List<Dictionary<int, Dictionary<int, Instant>>>();
@@ -62,11 +71,184 @@ namespace Intelektika
                 }
                 ApsimokykBajes(Data, o);
                 addProbab(visas);
+                KryzminePatikra(CheckList);
+           //}
+
+            /*for (int o = 1; o <= 10; o++)
+            {*/
                 
-            }
+                WinCount = 1;
+                DefeatCount = 0;
+                visas = new List<Dictionary<int, Dictionary<int, Instant>>>();
+                for (int i = 0; i < MaxNumberOfData; i++)//kintamuju skaicius
+                {
+                    Dictionary<int, Dictionary<int, Instant>> a = new Dictionary<int, Dictionary<int, Instant>>();
+                    for (int j = 0; j < MaxTime; j++)//laiko intervalai
+                    {
+                        Dictionary<int, Instant> b = new Dictionary<int, Instant>();
+                        a.Add(j, b);
+                    }
+                    visas.Add(a);
+                }
+                ApsimokykBajes2(Data, o);
+                addProbab(visas);
+                KryzminePatikra(CheckList);
+           //}
         }
+        
+        public void ApsimokykBajes2(List<string> list, int o)
+        {
+            CheckList = new List<List<int>>();
+            string line = null;
+            for (int i = 0; i < list.Count; i++)
+            {
+                sumazinta = 0;
+                line = list[i];
+                string[] values = line.Split(',');
+                if (i >= (o * LearnData / 10) || i < ((o - 1) * LearnData / 10))
+                {
+                    int j = 0;
 
+                    for (int l = 5; l < MaxNumberOfNormalizedData; l++)
+                    {
+                        visas = addUsingDimension(j, int.Parse(values[2]), int.Parse(values[l]), int.Parse(values[4]), visas);
+                        j++;
+                        if (int.Parse(values[4]) == 1)
+                        {
+                            WinCount++;
+                        }
+                        else
+                            DefeatCount++;
+                    }
+                }
+                else
+                {
+                    List<int> intList = new List<int>();
+                    intList.Add(int.Parse(values[2]));
+                    intList.Add(int.Parse(values[3]));
+                    intList.Add(int.Parse(values[4]));
+                    intList.Add(int.Parse(values[5]));
+                    intList.Add(int.Parse(values[6]));
+                    intList.Add(int.Parse(values[7]));
+                    intList.Add(int.Parse(values[8]));
+                    intList.Add(int.Parse(values[9]));
+                    intList.Add(int.Parse(values[10]));
+                    intList.Add(int.Parse(values[11]));
+                    intList.Add(int.Parse(values[12]));
+                    intList.Add(int.Parse(values[13]));
+                    intList.Add(int.Parse(values[14]));
+                    intList.Add(int.Parse(values[15]));
+                    intList.Add(int.Parse(values[16]));
+                    intList.Add(int.Parse(values[17]));
+                    intList.Add(int.Parse(values[18]));
+                    intList.Add(int.Parse(values[19]));
+                    intList.Add(int.Parse(values[20]));
+                    intList.Add(int.Parse(values[21]));
+                    intList.Add(int.Parse(values[22]));
+                    intList.Add(int.Parse(values[23]));
+                    intList.Add(int.Parse(values[24]));
+                    intList.Add(int.Parse(values[25]));
+                    intList.Add(int.Parse(values[26]));
+                    intList.Add(int.Parse(values[27]));
+                    intList.Add(int.Parse(values[28]));
+                    intList.Add(int.Parse(values[29]));
+                    intList.Add(int.Parse(values[30]));
+                    intList.Add(int.Parse(values[31]));
+                    intList.Add(int.Parse(values[32]));
+                    intList.Add(int.Parse(values[33]));
+                    intList.Add(int.Parse(values[34]));
+                    intList.Add(int.Parse(values[35]));
+                    intList.Add(int.Parse(values[36]));
+                    intList.Add(int.Parse(values[37]));
+                    intList.Add(int.Parse(values[38]));
+                    intList.Add(int.Parse(values[39]));
+                    intList.Add(int.Parse(values[40]));
+                    intList.Add(int.Parse(values[41]));
+                    intList.Add(int.Parse(values[42]));
+                    intList.Add(int.Parse(values[43]));
+                    intList.Add(int.Parse(values[44]));
+                    intList.Add(int.Parse(values[45]));
+                    intList.Add(int.Parse(values[46]));
+                    intList.Add(int.Parse(values[47]));
+                    intList.Add(int.Parse(values[48]));
+                    intList.Add(int.Parse(values[49]));
+                    intList.Add(int.Parse(values[50]));
+                    intList.Add(int.Parse(values[51]));
+                    intList.Add(int.Parse(values[52]));
+                    intList.Add(int.Parse(values[53]));
+                    intList.Add(int.Parse(values[54]));
+                    intList.Add(int.Parse(values[55]));
+                    intList.Add(int.Parse(values[56]));
+                    intList.Add(int.Parse(values[57]));
+                    intList.Add(int.Parse(values[58]));
+                    intList.Add(int.Parse(values[59]));
+                    intList.Add(int.Parse(values[60]));
 
+                    CheckList.Add(intList);
+                }
+            }
+
+        }
+        public void KryzminePatikra(List<List<int>> CheckList)
+        {
+            double truePositive = 0;
+            double falsePositive = 0;
+            foreach (var List in CheckList)
+            {
+                sumazinta = 0;
+                List<double> tikimybes = new List<double>();
+                for (int i = 0; i < List.Count-4; i++)
+                {
+                    tikimybes.Add(chekWinProb(i,List[i],List[2]));
+                }
+                List<double> topas = getTop(tikimybes);
+                double tikimybe = P(topas);
+                if (List[4] == 2 && tikimybe < slenkstis)
+                {
+                    truePositive++;
+                }
+                else if (List[4] == 1 && tikimybe > slenkstis)
+                {
+                    truePositive++;
+                }
+                else
+                    falsePositive++;
+            }
+            textBox1.Text += ("Rezultatai: " + (truePositive / (truePositive + falsePositive) * 100) + "%");
+        }
+        List<double> getTop(List<double> tikimybes)
+        {
+            List<Difference> diff = new List<Difference>();
+            for (int i = 0; i < tikimybes.Count; i++)
+            {
+                Difference tmp = new Difference();
+                tmp.Real = tikimybes[i];
+                if (tikimybes[i] > 0.5)
+                {
+                    tmp.Diff = 1 - tikimybes[i];
+                }
+                else
+                    tmp.Diff = tikimybes[i];
+                diff.Add(tmp);
+            }
+            List<Difference> sortedDiff = diff.OrderBy(o => o.Diff).ToList();
+            List<double> segmentas = new List<double>();
+            if (vertinamasTop <= sortedDiff.Count)
+            {
+                for (int i = 0; i < vertinamasTop; i++)
+                {
+                    segmentas.Add(sortedDiff[i].Real);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < sortedDiff.Count; i++)
+                {
+                    segmentas.Add(sortedDiff[i].Real);
+                }
+            }
+            return segmentas;
+        }
         static double P(List<double> topas)
         {
             double virsus = 1;
@@ -80,19 +262,34 @@ namespace Intelektika
             return (double)(virsus / (virsus + apacia2));
 
         }
-       
 
         public double chekWinProb(int eile, int variable, int time)
         {
-            if(visas[eile][SplitTime(time)].ContainsKey(variable))
+            int newEile = eile - sumazinta;
+            if (champions.Contains(eile))
             {
-                return visas[eile][SplitTime(time)].Single(e => e.Key == variable).Value.tikimybe;
+                newEile = champSpot;
+                sumazinta++;
+            }
+            else if (bans.Contains(eile))
+            {
+                newEile = banSpot;
+                sumazinta++;
+            }
+            else if (summonerSpell.Contains(eile))
+            {
+                newEile = summSpot;
+                sumazinta++;
+            }
+
+            if (visas[newEile][SplitTime(time)].ContainsKey(variable))
+            {
+                return visas[newEile][SplitTime(time)].Single(e => e.Key == variable).Value.tikimybe;
             }
             else
             return pirmas;
 
         }
-
 
         public int SplitTime(int time)
         {
@@ -109,7 +306,7 @@ namespace Intelektika
 
         public void ApsimokykBajes(List<string> list, int o)
         {
-            fullList = new List<List<int>>();
+            CheckList = new List<List<int>>();
             string line = null;
             for (int i = 0; i < list.Count; i++)
             {
@@ -194,7 +391,7 @@ namespace Intelektika
                     intList.Add(int.Parse(values[59]));
                     intList.Add(int.Parse(values[60]));
 
-                    fullList.Add(intList);
+                    CheckList.Add(intList);
                 }
             }
             
@@ -246,6 +443,89 @@ namespace Intelektika
             }
             return visas;
         }
+        public List<Dictionary<int, Dictionary<int, Instant>>> addUsingDimension(int eile, int time, int variable, int win, List<Dictionary<int, Dictionary<int, Instant>>> visas)
+        {
+            int newEile = eile - sumazinta;
+            if (champions.Contains(eile))
+            {
+                sumazinta++;
+
+                if (visas[champSpot][SplitTime(time)].ContainsKey(variable))
+                {
+                    if (win == 1)
+                        visas[champSpot][SplitTime(time)].Single(e => e.Key == variable).Value.t1_win++;
+                    else visas[champSpot][SplitTime(time)].Single(e => e.Key == variable).Value.t2_win++;
+                }
+                else
+                {
+                    Instant a = new Instant() { t1_win = 0, t2_win = 0, tikimybe = 0 };
+                    if (win == 1)
+                        a.t1_win++;
+                    else
+                        a.t2_win++;
+                    visas[champSpot][SplitTime(time)].Add(variable, a);
+                }
+            }
+            else if (bans.Contains(eile))
+            {
+                sumazinta++;
+
+                if (visas[banSpot][SplitTime(time)].ContainsKey(variable))
+                {
+                    if (win == 1)
+                        visas[banSpot][SplitTime(time)].Single(e => e.Key == variable).Value.t1_win++;
+                    else visas[banSpot][SplitTime(time)].Single(e => e.Key == variable).Value.t2_win++;
+                }
+                else
+                {
+                    Instant a = new Instant() { t1_win = 0, t2_win = 0, tikimybe = 0 };
+                    if (win == 1)
+                        a.t1_win++;
+                    else
+                        a.t2_win++;
+                    visas[banSpot][SplitTime(time)].Add(variable, a);
+                }
+            }
+            else if (summonerSpell.Contains(eile))
+            {
+                sumazinta++;
+
+                if (visas[summSpot][SplitTime(time)].ContainsKey(variable))
+                {
+                    if (win == 1)
+                        visas[summSpot][SplitTime(time)].Single(e => e.Key == variable).Value.t1_win++;
+                    else visas[summSpot][SplitTime(time)].Single(e => e.Key == variable).Value.t2_win++;
+                }
+                else
+                {
+                    Instant a = new Instant() { t1_win = 0, t2_win = 0, tikimybe = 0 };
+                    if (win == 1)
+                        a.t1_win++;
+                    else
+                        a.t2_win++;
+                    visas[summSpot][SplitTime(time)].Add(variable, a);
+                }
+            }
+            else
+            {
+                if (visas[newEile][SplitTime(time)].ContainsKey(variable))
+                {
+                    if (win == 1)
+                        visas[newEile][SplitTime(time)].Single(e => e.Key == variable).Value.t1_win++;
+                    else visas[newEile][SplitTime(time)].Single(e => e.Key == variable).Value.t2_win++;
+                }
+                else
+                {
+                    Instant a = new Instant() { t1_win = 0, t2_win = 0, tikimybe = 0 };
+                    if (win == 1)
+                        a.t1_win++;
+                    else
+                        a.t2_win++;
+                    visas[newEile][SplitTime(time)].Add(variable, a);
+                }
+            }
+            return visas;
+        }
         public List<string> ReadLinesAsString()
         {
             List<string> temp = new List<string>();
@@ -266,7 +546,7 @@ namespace Intelektika
 
         public List<List<int>> ReadLinesAsInt()
         {
-            List<List<int>> fullList = new List<List<int>>();
+            List<List<int>> CheckList = new List<List<int>>();
 
             using (StreamReader reader = new StreamReader(file))
             {
@@ -339,10 +619,10 @@ namespace Intelektika
                     intList.Add(int.Parse(values[60]));                   
                 }
 
-                fullList.Add(intList);
+                CheckList.Add(intList);
             }
 
-            return fullList;
+            return CheckList;
         }
     }
 
@@ -386,7 +666,11 @@ namespace Intelektika
         public int t2_champ4id { get; set; }
         public int t2_champ5id { get; set; }
     }
-    
 
+    class Difference
+    {
+        public double Real { get; set; }
+        public double Diff { get; set; }
+    }
 
 }
