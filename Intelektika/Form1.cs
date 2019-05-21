b22a15e7b3f28a7d91485e03d4efdb7b573cdc47
MissedSte4k <36638029+MissedSte4k@users.noreply.github.com>
2019-05-21 18:27:54 +0300

    Update Form1.cs

--- Intelektika/Form1.cs
+++ Intelektika/Form1.cs
@@ -20,9 +20,12 @@ namespace Intelektika
 
         public const int MaxTime = 16;
         public const int LearnData = 1000;
-        public List<FullData> fullDatas = new List<FullData>();
+        List<List<int>> fullList = new List<List<int>>();
         List<Dictionary<int, Dictionary<int, Instant>>> visas = new List<Dictionary<int, Dictionary<int, Instant>>>();
         public const int MaxNumberOfNormalizedData = 57;
+        public int WinCount = 0;
+        public int DefeatCount = 0;
+        public double pirmas = 0.5;
 
 
         public Form1()
@@ -44,8 +47,9 @@ namespace Intelektika
             
             for (int o = 1; o <= 10; o++)
             {
+                WinCount = 0;
+                DefeatCount = 0;
                 visas = new List<Dictionary<int, Dictionary<int, Instant>>>();
-                fullDatas = new List<FullData>();
                 for (int i = 0; i < MaxNumberOfNormalizedData; i++)//kintamuju skaicius
                 {
                     Dictionary<int, Dictionary<int, Instant>> a = new Dictionary<int, Dictionary<int, Instant>>();
@@ -60,95 +64,35 @@ namespace Intelektika
                 addProbab(visas);
                 
             }
-            List<FullData> fullDataList = ReadFile(file);
         }
 
-        public float chekWinProb()
-        {
-            float prob = 0;
-        }
 
-        public List<FullData> ReadFile(string file)
+        static double P(List<double> topas)
         {
-            List<FullData> fullDataList = new List<FullData>();
-
-            using (StreamReader reader = new StreamReader(file))
+            double virsus = 1;
+            double apacia2 = 1;
+            foreach (double top in topas)
             {
-                string line = null;
-                line = reader.ReadLine();
-
-                while (null != (line = reader.ReadLine()))
-                {
-                    FullData temp = new FullData();
-
-                    string[] values = line.Split(',');
-                    temp.GameID = double.Parse(values[0]);
-                    temp.CreationTime= double.Parse(values[1]);
-                    temp.GameDuration= int.Parse(values[2]);
-                    temp.Season= int.Parse(values[3]);
-                    temp.Winner= int.Parse(values[4]);
-                    temp.FirstBlood= int.Parse(values[5]);
-                    temp.FirstTower= int.Parse(values[6]);
-                    temp.FirstInhibitor= int.Parse(values[7]);
-                    temp.FirstBaron= int.Parse(values[8]);
-                    temp.FirstDragon= int.Parse(values[9]);
-                    temp.FirstRiftHerald= int.Parse(values[10]);
-                    temp.t1_champ1id= int.Parse(values[11]);
-                    temp.t1_champ1_sum1= int.Parse(values[12]);
-                    temp.t1_champ1_sum2= int.Parse(values[13]);
-                    temp.t1_champ2id= int.Parse(values[14]);
-                    temp.t1_champ2_sum1= int.Parse(values[15]);
-                    temp.t1_champ2_sum2= int.Parse(values[16]);
-                    temp.t1_champ3id= int.Parse(values[17]);
-                    temp.t1_champ3_sum1= int.Parse(values[18]);
-                    temp.t1_champ3_sum2= int.Parse(values[19]);
-                    temp.t1_champ4id= int.Parse(values[20]);
-                    temp.t1_champ4_sum1= int.Parse(values[21]);
-                    temp.t1_champ4_sum2= int.Parse(values[22]);
-                    temp.t1_champ5id= int.Parse(values[23]);
-                    temp.t1_champ5_sum1= int.Parse(values[24]);
-                    temp.t1_champ5_sum2= int.Parse(values[25]);
-                    temp.t1_towerKill= int.Parse(values[26]);
-                    temp.t1_inhibitorKills= int.Parse(values[27]);
-                    temp.t1_baronKills= int.Parse(values[28]);
-                    temp.t1_dragonKills= int.Parse(values[29]);
-                    temp.t1_riftHeraldKills= int.Parse(values[30]);
-                    temp.t1_ban1= int.Parse(values[31]);
-                    temp.t1_ban2= int.Parse(values[32]);
-                    temp.t1_ban3= int.Parse(values[33]);
-                    temp.t1_ban4= int.Parse(values[34]);
-                    temp.t1_ban5= int.Parse(values[35]);
-                    temp.t2_champ1id= int.Parse(values[36]);
-                    temp.t2_champ1_sum1= int.Parse(values[37]);
-                    temp.t2_champ1_sum2= int.Parse(values[38]);
-                    temp.t2_champ2id= int.Parse(values[39]);
-                    temp.t2_champ2_sum1= int.Parse(values[40]);
-                    temp.t2_champ2_sum2= int.Parse(values[41]);
-                    temp.t2_champ3id= int.Parse(values[42]);
-                    temp.t2_champ3_sum1= int.Parse(values[43]);
-                    temp.t2_champ3_sum2= int.Parse(values[44]);
-                    temp.t2_champ4id= int.Parse(values[45]);
-                    temp.t2_champ4_sum1= int.Parse(values[46]);
-                    temp.t2_champ4_sum2= int.Parse(values[47]);
-                    temp.t2_champ5id= int.Parse(values[48]);
-                    temp.t2_champ5_sum1= int.Parse(values[49]);
-                    temp.t2_champ5_sum2= int.Parse(values[50]);
-                    temp.t2_towerKill= int.Parse(values[51]);
-                    temp.t2_inhibitorKills= int.Parse(values[52]);
-                    temp.t2_baronKills= int.Parse(values[53]);
-                    temp.t2_dragonKills= int.Parse(values[54]);
-                    temp.t2_riftHeraldKills= int.Parse(values[55]);
-                    temp.t2_ban1= int.Parse(values[56]);
-                    temp.t2_ban2= int.Parse(values[57]);
-                    temp.t2_ban3= int.Parse(values[58]);
-                    temp.t2_ban4= int.Parse(values[59]);
-                    temp.t2_ban5= int.Parse(values[60]);
-
-                    fullDataList.Add(temp);
-                }
+                virsus = (double)virsus * top;
+                apacia2 = (double)apacia2 * (1 - top);
             }
-            return fullDataList;
+
+            return (double)(virsus / (virsus + apacia2));
+
         }
+       
+
+        public double chekWinProb(int eile, int variable, int time)
+        {
+            if(visas[eile][SplitTime(time)].ContainsKey(variable))
+            {
+                return visas[eile][SplitTime(time)].Single(e => e.Key == variable).Value.tikimybe;
+            }
+            else
+            return pirmas;
+
+        }
+
 
         public int SplitTime(int time)
         {
@@ -165,10 +109,11 @@ namespace Intelektika
 
         public void ApsimokykBajes(List<string> list, int o)
         {
-                    string line = null;
+            fullList = new List<List<int>>();
+            string line = null;
             for (int i = 0; i < list.Count; i++)
             {
-                line = list[o];
+                line = list[i];
                 string[] values = line.Split(',');
                 if (i >= (o * LearnData / 10) || i < ((o - 1) * LearnData / 10))
                 {
@@ -178,79 +123,83 @@ namespace Intelektika
                     {
                         visas = add(j, int.Parse(values[2]), int.Parse(values[l]), int.Parse(values[4]), visas);
                         j++;
+                        if(int.Parse(values[4]) == 1)
+                        {
+                            WinCount++;
+                        }
+                        else
+                            DefeatCount++;
                     }
                 }
                 else
                 {
-                    FullData temp = new FullData();
+                    List<int> intList = new List<int>();
+                    intList.Add(int.Parse(values[2]));
+                    intList.Add(int.Parse(values[3]));
+                    intList.Add(int.Parse(values[4]));
+                    intList.Add(int.Parse(values[5]));
+                    intList.Add(int.Parse(values[6]));
+                    intList.Add(int.Parse(values[7]));
+                    intList.Add(int.Parse(values[8]));
+                    intList.Add(int.Parse(values[9]));
+                    intList.Add(int.Parse(values[10]));
+                    intList.Add(int.Parse(values[11]));
+                    intList.Add(int.Parse(values[12]));
+                    intList.Add(int.Parse(values[13]));
+                    intList.Add(int.Parse(values[14]));
+                    intList.Add(int.Parse(values[15]));
+                    intList.Add(int.Parse(values[16]));
+                    intList.Add(int.Parse(values[17]));
+                    intList.Add(int.Parse(values[18]));
+                    intList.Add(int.Parse(values[19]));
+                    intList.Add(int.Parse(values[20]));
+                    intList.Add(int.Parse(values[21]));
+                    intList.Add(int.Parse(values[22]));
+                    intList.Add(int.Parse(values[23]));
+                    intList.Add(int.Parse(values[24]));
+                    intList.Add(int.Parse(values[25]));
+                    intList.Add(int.Parse(values[26]));
+                    intList.Add(int.Parse(values[27]));
+                    intList.Add(int.Parse(values[28]));
+                    intList.Add(int.Parse(values[29]));
+                    intList.Add(int.Parse(values[30]));
+                    intList.Add(int.Parse(values[31]));
+                    intList.Add(int.Parse(values[32]));
+                    intList.Add(int.Parse(values[33]));
+                    intList.Add(int.Parse(values[34]));
+                    intList.Add(int.Parse(values[35]));
+                    intList.Add(int.Parse(values[36]));
+                    intList.Add(int.Parse(values[37]));
+                    intList.Add(int.Parse(values[38]));
+                    intList.Add(int.Parse(values[39]));
+                    intList.Add(int.Parse(values[40]));
+                    intList.Add(int.Parse(values[41]));
+                    intList.Add(int.Parse(values[42]));
+                    intList.Add(int.Parse(values[43]));
+                    intList.Add(int.Parse(values[44]));
+                    intList.Add(int.Parse(values[45]));
+                    intList.Add(int.Parse(values[46]));
+                    intList.Add(int.Parse(values[47]));
+                    intList.Add(int.Parse(values[48]));
+                    intList.Add(int.Parse(values[49]));
+                    intList.Add(int.Parse(values[50]));
+                    intList.Add(int.Parse(values[51]));
+                    intList.Add(int.Parse(values[52]));
+                    intList.Add(int.Parse(values[53]));
+                    intList.Add(int.Parse(values[54]));
+                    intList.Add(int.Parse(values[55]));
+                    intList.Add(int.Parse(values[56]));
+                    intList.Add(int.Parse(values[57]));
+                    intList.Add(int.Parse(values[58]));
+                    intList.Add(int.Parse(values[59]));
+                    intList.Add(int.Parse(values[60]));
 
-                    temp.GameID = double.Parse(values[0]);
-                    temp.CreationTime = double.Parse(values[1]);
-                    temp.GameDuration = int.Parse(values[2]);
-                    temp.Season = int.Parse(values[3]);
-                    temp.Winner = int.Parse(values[4]);
-                    temp.FirstBlood = int.Parse(values[5]);
-                    temp.FirstTower = int.Parse(values[6]);
-                    temp.FirstInhibitor = int.Parse(values[7]);
-                    temp.FirstBaron = int.Parse(values[8]);
-                    temp.FirstDragon = int.Parse(values[9]);
-                    temp.FirstRiftHerald = int.Parse(values[10]);
-                    temp.t1_champ1id = int.Parse(values[11]);
-                    temp.t1_champ1_sum1 = int.Parse(values[12]);
-                    temp.t1_champ1_sum2 = int.Parse(values[13]);
-                    temp.t1_champ2id = int.Parse(values[14]);
-                    temp.t1_champ2_sum1 = int.Parse(values[15]);
-                    temp.t1_champ2_sum2 = int.Parse(values[16]);
-                    temp.t1_champ3id = int.Parse(values[17]);
-                    temp.t1_champ3_sum1 = int.Parse(values[18]);
-                    temp.t1_champ3_sum2 = int.Parse(values[19]);
-                    temp.t1_champ4id = int.Parse(values[20]);
-                    temp.t1_champ4_sum1 = int.Parse(values[21]);
-                    temp.t1_champ4_sum2 = int.Parse(values[22]);
-                    temp.t1_champ5id = int.Parse(values[23]);
-                    temp.t1_champ5_sum1 = int.Parse(values[24]);
-                    temp.t1_champ5_sum2 = int.Parse(values[25]);
-                    temp.t1_towerKill = int.Parse(values[26]);
-                    temp.t1_inhibitorKills = int.Parse(values[27]);
-                    temp.t1_baronKills = int.Parse(values[28]);
-                    temp.t1_dragonKills = int.Parse(values[29]);
-                    temp.t1_riftHeraldKills = int.Parse(values[30]);
-                    temp.t1_ban1 = int.Parse(values[31]);
-                    temp.t1_ban2 = int.Parse(values[32]);
-                    temp.t1_ban3 = int.Parse(values[33]);
-                    temp.t1_ban4 = int.Parse(values[34]);
-                    temp.t1_ban5 = int.Parse(values[35]);
-                    temp.t2_champ1id = int.Parse(values[36]);
-                    temp.t2_champ1_sum1 = int.Parse(values[37]);
-                    temp.t2_champ1_sum2 = int.Parse(values[38]);
-                    temp.t2_champ2id = int.Parse(values[39]);
-                    temp.t2_champ2_sum1 = int.Parse(values[40]);
-                    temp.t2_champ2_sum2 = int.Parse(values[41]);
-                    temp.t2_champ3id = int.Parse(values[42]);
-                    temp.t2_champ3_sum1 = int.Parse(values[43]);
-                    temp.t2_champ3_sum2 = int.Parse(values[44]);
-                    temp.t2_champ4id = int.Parse(values[45]);
-                    temp.t2_champ4_sum1 = int.Parse(values[46]);
-                    temp.t2_champ4_sum2 = int.Parse(values[47]);
-                    temp.t2_champ5id = int.Parse(values[48]);
-                    temp.t2_champ5_sum1 = int.Parse(values[49]);
-                    temp.t2_champ5_sum2 = int.Parse(values[50]);
-                    temp.t2_towerKill = int.Parse(values[51]);
-                    temp.t2_inhibitorKills = int.Parse(values[52]);
-                    temp.t2_baronKills = int.Parse(values[53]);
-                    temp.t2_dragonKills = int.Parse(values[54]);
-                    temp.t2_riftHeraldKills = int.Parse(values[55]);
-                    temp.t2_ban1 = int.Parse(values[56]);
-                    temp.t2_ban2 = int.Parse(values[57]);
-                    temp.t2_ban3 = int.Parse(values[58]);
-                    temp.t2_ban4 = int.Parse(values[59]);
-                    temp.t2_ban5 = int.Parse(values[60]);
-
-                    fullDatas.Add(temp);
+                    fullList.Add(intList);
                 }
             }
             
         }
+        
 
         public List<Dictionary<int, Dictionary<int, Instant>>> addProbab(List<Dictionary<int, Dictionary<int, Instant>>> visas)
         {
@@ -260,7 +209,18 @@ namespace Intelektika
                 {
                     foreach (Instant instant in item.Values)
                     {
-                        instant.tikimybe = (float) (instant.t1_win / (LearnData / 10 * 9) / (instant.t1_win/(LearnData/10*9) + instant.t2_win / (LearnData / 10 * 9)));
+                        double ws = (double) instant.t1_win / WinCount;
+                        double wh = (double) instant.t2_win / DefeatCount;
+                        double tik = (double)ws / (ws + wh);
+                        if (ws == 0)
+                        {
+                            tik = 0.01;
+                        }
+                        if (wh == 0)
+                        {
+                            tik = 0.99;
+                        }
+                        instant.tikimybe = tik;
                     }
                 }
             }
@@ -392,7 +352,7 @@ namespace Intelektika
     {
         public int t1_win { get; set; }
         public int t2_win { get; set; }
-        public float tikimybe { get; set; }
+        public double tikimybe { get; set; }
     }
 
    public class NormilizedData
@@ -426,70 +386,7 @@ namespace Intelektika
         public int t2_champ4id { get; set; }
         public int t2_champ5id { get; set; }
     }
-    public class FullData
-    {
-        public double GameID { get; set; }
-        public double CreationTime { get; set; }
-        public int GameDuration { get; set; }
-        public int Season { get; set; }
-        public int Winner { get; set; }
-        public int FirstBlood { get; set; }
-        public int FirstTower { get; set; }
-        public int FirstInhibitor { get; set; }
-        public int FirstBaron { get; set; }
-        public int FirstDragon { get; set; }
-        public int FirstRiftHerald { get; set; }
-        public int t1_champ1id { get; set; }
-        public int t1_champ1_sum1 { get; set; }
-        public int t1_champ1_sum2 { get; set; }
-        public int t1_champ2id { get; set; }
-        public int t1_champ2_sum1 { get; set; }
-        public int t1_champ2_sum2 { get; set; }
-        public int t1_champ3id { get; set; }
-        public int t1_champ3_sum1 { get; set; }
-        public int t1_champ3_sum2 { get; set; }
-        public int t1_champ4id { get; set; }
-        public int t1_champ4_sum1 { get; set; }
-        public int t1_champ4_sum2 { get; set; }
-        public int t1_champ5id { get; set; }
-        public int t1_champ5_sum1 { get; set; }
-        public int t1_champ5_sum2 { get; set; }
-        public int t1_towerKill { get; set; }
-        public int t1_inhibitorKills { get; set; }
-        public int t1_baronKills { get; set; }
-        public int t1_dragonKills { get; set; }
-        public int t1_riftHeraldKills { get; set; }
-        public int t1_ban1 { get; set; }
-        public int t1_ban2 { get; set; }
-        public int t1_ban3 { get; set; }
-        public int t1_ban4 { get; set; }
-        public int t1_ban5 { get; set; }
-        public int t2_champ1id { get; set; }
-        public int t2_champ1_sum1 { get; set; }
-        public int t2_champ1_sum2 { get; set; }
-        public int t2_champ2id { get; set; }
-        public int t2_champ2_sum1 { get; set; }
-        public int t2_champ2_sum2 { get; set; }
-        public int t2_champ3id { get; set; }
-        public int t2_champ3_sum1 { get; set; }
-        public int t2_champ3_sum2 { get; set; }
-        public int t2_champ4id { get; set; }
-        public int t2_champ4_sum1 { get; set; }
-        public int t2_champ4_sum2 { get; set; }
-        public int t2_champ5id { get; set; }
-        public int t2_champ5_sum1 { get; set; }
-        public int t2_champ5_sum2 { get; set; }
-        public int t2_towerKill { get; set; }
-        public int t2_inhibitorKills { get; set; }
-        public int t2_baronKills { get; set; }
-        public int t2_dragonKills { get; set; }
-        public int t2_riftHeraldKills { get; set; }
-        public int t2_ban1 { get; set; }
-        public int t2_ban2 { get; set; }
-        public int t2_ban3 { get; set; }
-        public int t2_ban4 { get; set; }
-        public int t2_ban5 { get; set; }
-    }
+    
 
 
 }
