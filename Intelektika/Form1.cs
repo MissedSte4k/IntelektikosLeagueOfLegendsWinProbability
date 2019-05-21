93910972d357ac0b8b22ff4ffedd5f1d818247ba
MissedSte4k <36638029+MissedSte4k@users.noreply.github.com>
2019-05-21 13:43:09 +0300

    first

---
+++ Intelektika/Form1.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// This is the code for your desktop app.
+// Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+
+namespace Intelektika
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Click on the link below to continue learning how to build a desktop app using WinForms!
+            System.Diagnostics.Process.Start("http://aka.ms/dotnet-get-started-desktop");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Thanks!");
+        }
+    }
+
+    public class 
+    public class FullData
+    {
+        public int GameID { get; set; }
+        public int CreationTime { get; set; }
+        public int GameDuration { get; set; }
+        public int Season { get; set; }
+        public int Winner { get; set; }
+        public int FirstBlood { get; set; }
+        public int FirstTower { get; set; }
+        public int FirstInhibitor { get; set; }
+        public int FirstBaron { get; set; }
+        public int FirstDragon { get; set; }
+        public int FirstRiftHerald { get; set; }
+        public int t1_champ1id { get; set; }
+        public int t1_champ1_sum1 { get; set; }
+        public int t1_champ1_sum2 { get; set; }
+        public int t1_champ2id { get; set; }
+        public int t1_champ2_sum1 { get; set; }
+        public int t1_champ2_sum2 { get; set; }
+        public int t1_champ3id { get; set; }
+        public int t1_champ3_sum1 { get; set; }
+        public int t1_champ3_sum2 { get; set; }
+        public int t1_champ4id { get; set; }
+        public int t1_champ4_sum1 { get; set; }
+        public int t1_champ4_sum2 { get; set; }
+        public int t1_champ5id { get; set; }
+        public int t1_champ5_sum1 { get; set; }
+        public int t1_champ5_sum2 { get; set; }
+        public int t1_towerKill { get; set; }
+        public int t1_inhibitorKills { get; set; }
+        public int t1_baronKills { get; set; }
+        public int t1_dragonKills { get; set; }
+        public int t1_riftHeraldKills { get; set; }
+        public int t1_ban1 { get; set; }
+        public int t1_ban2 { get; set; }
+        public int t1_ban3 { get; set; }
+        public int t1_ban4 { get; set; }
+        public int t1_ban5 { get; set; }
+        public int t2_champ1id { get; set; }
+        public int t2_champ1_sum1 { get; set; }
+        public int t2_champ1_sum2 { get; set; }
+        public int t2_champ2id { get; set; }
+        public int t2_champ2_sum1 { get; set; }
+        public int t2_champ2_sum2 { get; set; }
+        public int t2_champ3id { get; set; }
+        public int t2_champ3_sum1 { get; set; }
+        public int t2_champ3_sum2 { get; set; }
+        public int t2_champ4id { get; set; }
+        public int t2_champ4_sum1 { get; set; }
+        public int t2_champ4_sum2 { get; set; }
+        public int t2_champ5id { get; set; }
+        public int t2_champ5_sum1 { get; set; }
+        public int t2_champ5_sum2 { get; set; }
+        public int t2_towerKill { get; set; }
+        public int t2_inhibitorKills { get; set; }
+        public int t2_baronKills { get; set; }
+        public int t2_dragonKills { get; set; }
+        public int t2_riftHeraldKills { get; set; }
+        public int t2_ban1 { get; set; }
+        public int t2_ban2 { get; set; }
+        public int t2_ban3 { get; set; }
+        public int t2_ban4 { get; set; }
+        public int t2_ban5 { get; set; }
+    }
+
+
+}
